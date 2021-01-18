2c5ba2c50dcdc2a4edef999f14c2037f3febb6cb
Afsin Ustundag <afsin_ustundag@yahoo.com>
2021-01-17 19:55:44 -0500

    code cleaning

--- test/util/shared-spec.js
+++ test/util/shared-spec.js
@@ -50,21 +50,5 @@ export default class SharedSpec {
     expectedSeqErrorHandler(name, fields) { // eslint-disable-line class-methods-use-this
         return errHandler.expectedSeqErrorHandlerFn(name, fields);
     }
-
-    sanityEnoughUserTested(hxUser) { // eslint-disable-line class-methods-use-this
-        return function sanityEnoughUserTested() {
-            const userCount = hxUser.length();
-            const counts = _.range(userCount).reduce((r, index) => {
-                if (hxUser.client(index).username) {
-                    r.username += 1;
-                } else {
-                    r.email += 1;
-                }
-                return r;
-            }, { username: 0, email: 0 });
-            expect(counts.username).to.be.above(0);
-            expect(counts.email).to.be.above(0);
-        };
-    }
 }
 
